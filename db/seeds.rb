a0571296a0d222b585ce05bc21dfa368a15ebbcc
rishijavia <rishi.javia.3895@gmail.com>
2016-10-03 16:32:54 -0700

    added partials for events listing table and creating seeds for events

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+event1 = Event.create(name: 'Test Event 1', start_date: '2016-10-15', finish_date: '2016-10-28', start_location: 'Portland, OR',
+            end_location: 'Seattle, WA', createdby: 1, invitecode: 'M1BFVZ', private: false, team_event: false)
+event2 = Event.create(name: 'Test Event 2', start_date: '2016-09-01', finish_date: '2016-09-14', start_location: 'Milwaukee, WI',
+            end_location: 'Madison, WI', createdby: 1, invitecode: 'P5VFBA', private: true, team_event: false)
+event3 = Event.create(name: 'Test Event 3', start_date: '2016-09-28', finish_date: '2016-10-11', start_location: 'Mumbai',
+            end_location: 'Rajkot', createdby: 1, invitecode: 'M153KS', private: false, team_event: true)
+event4 = Event.create(name: 'Test Event 4', start_date: '2016-09-28', finish_date: '2016-10-11', start_location: 'Kolkota',
+            end_location: 'Delhi', createdby: 1, invitecode: 'LAS0FL1', private: true, team_event: true)
+event5 = Event.create(name: 'Test Event 5', start_date: '2016-10-28', finish_date: '2016-11-19', start_location: 'Paris',
+            end_location: 'Nice', createdby: 2, invitecode: 'MK30PK', private: false, team_event: true)
+event6 = Event.create(name: 'Test Event 6', start_date: '2016-09-28', finish_date: '2016-10-11', start_location: 'Berlin',
+            end_location: 'Munich', createdby: 2, invitecode: 'TLA03K', private: true, team_event: false)
+UserEvent.create(user_id: 1, event_id: event2.id)
+UserEvent.create(user_id: 1, event_id: event6.id)
+UserEvent.create(user_id: 2, event_id: event2.id)
+UserEvent.create(user_id: 2, event_id: event5.id)
+UserEvent.create(user_id: 2, event_id: event1.id)
