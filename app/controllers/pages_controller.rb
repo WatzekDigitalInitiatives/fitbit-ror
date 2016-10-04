20f144c5ed5ae9d769f47c47d8fcf9d47078e19e
budak <thatbudakguy@gmail.com>
2016-10-04 15:40:07 -0700

    style splash page
    refs #2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,17 @@
 class PagesController < ApplicationController
-   before_action :authenticate_user!, :except => [:home, :about]
+    before_action :authenticate_user!, except: [:home, :about]
 
+    def home
+        redirect_to dashboard_path if user_signed_in?
+    end
 
-   def home
-     if user_signed_in?
-       redirect_to dashboard_path
-     end
-   end
+    def dashboard
+        @user = current_user
+        @participating_events = current_user.events
+        @user_id = current_user.id
+        @user_events = Event.where(createdby: @user_id)
+    end
 
-   def dashboard
-     @user = current_user
-     @participating_events = current_user.events
-     @user_id = current_user.id
-     @user_events = Event.where(createdby: @user_id)
-   end
-
+    def about
+    end
 end
