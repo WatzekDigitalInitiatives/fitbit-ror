833bcfcfa63cf2b2a37f2d584ffb025e0f6b56e7
Rishi Javia <rishi.javia.3895@gmail.com>
2016-11-21 17:14:03 -0800

    moving stuff around

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,15 +8,16 @@ class PagesController < ApplicationController
     def dashboard
         @user = current_user
         client = @user.fitbit_client
+
+        output_goals = client.goals('daily')
+        hash = JSON.parse(output_goals.to_json)
+        @goal = hash["goals"]["steps"]
+
         user_tmz = current_user.identity_for("fitbit").timezone
         today = Date.today.in_time_zone(user_tmz).to_date.strftime("%Y-%m-%d")
         output_steps = client.activity_time_series(resource: 'steps', start_date: today, period: '1d')
         hash = JSON.parse(output_steps.to_json)
         @steps = hash["activities-steps"][0]["value"]
-
-        output_goals = client.goals('daily')
-        hash = JSON.parse(output_goals.to_json)
-        @goal = hash["goals"]["steps"]
         # render json: output_goals
 
         # show user subscriptions:
