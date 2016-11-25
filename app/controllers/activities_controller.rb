324f19ab9e9655d03fa1a7cb71701d85c2bf71fe
rishijavia <rishi.javia.3895@gmail.com>
2016-11-25 13:50:26 -0800

    changing the checking method

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -44,7 +44,7 @@ class ActivitiesController < ApplicationController
 
     @activity = Activity.find_by(entry_date: today, user_id: @user.id)
 
-    if @activity && !@activity.empty?
+    if @activity
       # update that activity
       @activity.steps = @steps
       @activity.save
