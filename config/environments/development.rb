afc272db0f253e5616f7b882f35ea6ebfaeb761d
rishijavia <rishi.javia.3895@gmail.com>
2017-01-31 20:27:23 -0800

    not using s3 with paperclip in dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,14 +41,4 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
-      s3_host_name: 's3-us-west-2.amazonaws.com'
-    }
-  }
 end
