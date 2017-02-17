c87074f7b24202671888e36ae626956630bbf2cc
Rishi Javia <rishi.javia.3895@gmail.com>
2017-02-17 15:08:40 -0800

    removed refresh token on login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def fitbit
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      refresh_user_token(@user.id)
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Fitbit") if is_navigational_format?
     else
