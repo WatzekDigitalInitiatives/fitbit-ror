ce47d50f6d457b38e275f6cd43bde75443743174
Rishi Javia <rishi.javia.3895@gmail.com>
2017-05-21 13:41:12 -0700

    fixed user date subs, stupid to code at 3am drunk

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -150,7 +150,7 @@ class EventsController < ApplicationController
                         @team = Team.find(params[:team_id])
                         @team.users.each do |user|
                             set_subscription_date(user.id, @event.start_date, @event.finish_date)
-                            create_user_subscription(user) if current_user.events.count == 1 && Rails.env.production?
+                            create_user_subscription(user) if user.events.count == 1 && Rails.env.production?
                         end
                         format.html { redirect_to @event, notice: 'Event was successfully created.' }
                         format.json { render :show, status: :created, location: @event }
@@ -161,7 +161,7 @@ class EventsController < ApplicationController
                     @user_event.event_id = @event.id
                     if @user_event.save
                         set_subscription_date(current_user.id, @event.start_date, @event.finish_date)
-                        create_user_subscription(current_user) if user.events.count == 1 && Rails.env.production?
+                        create_user_subscription(current_user) if current_user.events.count == 1 && Rails.env.production?
                         format.html { redirect_to @event, notice: 'Event was successfully created.' }
                         format.json { render :show, status: :created, location: @event }
                     end
